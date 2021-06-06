f8c1263ad6592608731b3e5e72b5799d90eddb14
speed <speed@DESKTOP-GBB5EQH>
2021-06-06 21:47:27 +0300

    before chainging joystick

---
+++ app/src/main/java/com/journaldev/androidmvvmbasics/views/myJoy.java
@@ -0,0 +1,16 @@
+package com.journaldev.androidmvvmbasics.views;
+
+import android.content.Context;
+import android.view.View;
+
+public class myJoy extends View {
+    /**
+     * Simple constructor to use when creating a view from code.
+     *
+     * @param context The Context the view is running in, through which it can
+     *                access the current theme, resources, etc.
+     */
+    public myJoy(Context context) {
+        super(context);
+    }
+}
